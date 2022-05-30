bd3e09d25c57b9709babd9a806d91aca4ba5f46f
BogdanParashchak <bogdan1986@ukr.net>
2022-05-30 20:35:07 +0300

    Refactor UserController
    1. Refactor UserController methods: parameters, return type, annotations used

--- src/main/java/com/parashchak/onlineshop/controller/UserController.java
+++ src/main/java/com/parashchak/onlineshop/controller/UserController.java
@@ -23,64 +23,61 @@ public class UserController {
     private ProductService productService;
 
     @RequestMapping(path = "/products", method = RequestMethod.GET)
-    public void getAll(HttpServletResponse response) throws IOException {
-        showAll(response);
+    @ResponseBody
+    public String getAll() {
+        return showAll();
     }
 
     @RequestMapping(path = "/products/add", method = RequestMethod.GET)
-    public void loadAddPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String page = templater.getPage("addPage.html");
-        response.getWriter().write(page);
+    @ResponseBody
+    public String loadAddPage() {
+        return templater.getPage("addPage.html");
     }
 
     @RequestMapping(path = "/products/add", method = RequestMethod.POST)
-    public void add(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @ResponseBody
+    public String add(HttpServletRequest request) throws IOException {
         Product product = toProduct(request);
         productService.add(product);
-        String page = templater.getPage("addPageSuccessful.html");
-        response.getWriter().write(page);
+        return templater.getPage("addPageSuccessful.html");
     }
 
     @RequestMapping(path = "/products/search", method = RequestMethod.GET)
-    public void search(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Map<String, Object> pageData = new HashMap<>();
-        String searchText = request.getParameter("search-text");
+    @ResponseBody
+    public String search(@RequestParam("search-text") String searchText) {
         List<Product> productsSearchList = productService.search(searchText);
+        Map<String, Object> pageData = new HashMap<>();
         pageData.put("products", productsSearchList);
-        String page = templater.getPage("viewPage.html", pageData);
-        response.getWriter().write(page);
+        return templater.getPage("viewPage.html", pageData);
     }
 
     @RequestMapping(path = "/products/edit", method = RequestMethod.GET)
-    public void getById(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @ResponseBody
+    public String getById(@RequestParam("id") int userId) {
+        Product product = productService.getById(userId).orElseThrow();
         Map<String, Object> pageData = new HashMap<>();
-        int id = Integer.parseInt(request.getParameter("id"));
-        Product product = productService.getById(id).orElseThrow();
         pageData.put("product", product);
-        String page = templater.getPage("editPage.html", pageData);
-        response.getWriter().write(page);
+        return templater.getPage("editPage.html", pageData);
     }
 
     @RequestMapping(path = "/products/edit", method = RequestMethod.POST)
-    public void edit(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @ResponseBody
+    public String edit(HttpServletRequest request) {
         Product product = toProduct(request);
         productService.update(product);
-        String page = templater.getPage("editPageSuccessful.html");
-        response.getWriter().write(page);
+        return templater.getPage("editPageSuccessful.html");
     }
 
     @RequestMapping(path = "/products/delete", method = RequestMethod.GET)
-    public void delete(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        int id = Integer.parseInt(request.getParameter("id"));
-        productService.delete(id);
+    public void delete(@RequestParam("id") int userId, HttpServletResponse response) throws IOException {
+        productService.delete(userId);
         response.sendRedirect("/products");
     }
 
-    private void showAll(HttpServletResponse response) throws IOException {
-        Map<String, Object> pageData = new HashMap<>();
+    private String showAll() {
         List<Product> allProducts = productService.getAll();
+        Map<String, Object> pageData = new HashMap<>();
         pageData.put("products", allProducts);
-        String page = templater.getPage("viewPage.html", pageData);
-        response.getWriter().write(page);
+        return templater.getPage("viewPage.html", pageData);
     }
 }
--- src/main/java/com/parashchak/onlineshop/service/UserService.java
+++ src/main/java/com/parashchak/onlineshop/service/UserService.java
@@ -1,10 +1,8 @@
 package com.parashchak.onlineshop.service;
 
 import com.parashchak.onlineshop.dao.UserDao;
-import com.parashchak.onlineshop.dao.jdbc.JdbcUserDao;
 import com.parashchak.onlineshop.entity.User;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
 
