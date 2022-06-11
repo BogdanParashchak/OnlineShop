fb14a69638c569bbf9a40f42d587c3fbd5831e42
BogdanParashchak <bogdan1986@ukr.net>
2022-06-11 14:07:41 +0300

    Refactor JdbcProductDao for using of JDBCTemplate

--- src/main/java/com/parashchak/onlineshop/dao/jdbc/JdbcProductDao.java
+++ src/main/java/com/parashchak/onlineshop/dao/jdbc/JdbcProductDao.java
@@ -1,123 +1,56 @@
 package com.parashchak.onlineshop.dao.jdbc;
 
 import com.parashchak.onlineshop.dao.ProductDao;
-import com.parashchak.onlineshop.dao.jdbc.mapper.ProductRowMapper;
 import com.parashchak.onlineshop.entity.Product;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.*;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
-import java.sql.*;
 import java.util.*;
 
 @Repository
 @Slf4j
 public class JdbcProductDao implements ProductDao {
 
-    private static final ProductRowMapper PRODUCT_ROW_MAPPER = new ProductRowMapper();
-
     private static final String GET_ALL_PRODUCTS_QUERY = "SELECT id,name, price, creation_date, description FROM products ORDER BY id";
     private static final String ADD_PRODUCT_QUERY = "INSERT INTO products (name, price, creation_date, description) VALUES (?, ?, ?, ?)";
     private static final String DELETE_PRODUCT_QUERY = "DELETE FROM products WHERE id=?";
     private static final String GET_PRODUCT_BY_ID_QUERY = "SELECT id, name, price, creation_date, description FROM products WHERE id=?";
     private static final String UPDATE_PRODUCT_QUERY = "UPDATE products SET name=?,price=?, description=? WHERE id=?";
-    private static final String SEARCH_PRODUCTS_QUERY = "SELECT id,name, price, creation_date, description FROM products WHERE name LIKE CONCAT( '%',?,'%') OR description LIKE CONCAT( '%',?,'%')";
+    private static final String SEARCH_PRODUCTS_QUERY = "SELECT * FROM products WHERE name LIKE CONCAT( '%',?,'%') OR description LIKE CONCAT( '%',?,'%')";
 
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public JdbcProductDao(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public JdbcProductDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<Product> getAll() {
-        try (Connection connection = dataSource.getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(GET_ALL_PRODUCTS_QUERY)) {
-            List<Product> allProductsList = new ArrayList<>();
-            while (resultSet.next()) {
-                Product product = PRODUCT_ROW_MAPPER.mapRow(resultSet);
-                allProductsList.add(product);
-            }
-            log.info("Executed: {}", GET_ALL_PRODUCTS_QUERY);
-            return allProductsList;
-        } catch (Exception e) {
-            throw new RuntimeException("Unable to findByLogin all products list from DB", e);
-        }
+        return jdbcTemplate.query(GET_ALL_PRODUCTS_QUERY, new BeanPropertyRowMapper<>(Product.class), null);
     }
 
     public void add(Product product) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(ADD_PRODUCT_QUERY)) {
-            preparedStatement.setString(1, product.getName());
-            preparedStatement.setDouble(2, product.getPrice());
-            preparedStatement.setTimestamp(3, Timestamp.valueOf(product.getCreationDate()));
-            preparedStatement.setString(4, product.getDescription());
-            preparedStatement.executeUpdate();
-            log.info("Executed: {}", preparedStatement);
-        } catch (Exception e) {
-            throw new RuntimeException("Unable to add product to DB", e);
-        }
+        jdbcTemplate.update(ADD_PRODUCT_QUERY, product.getName(), product.getPrice(),
+                product.getCreationDate(), product.getDescription());
     }
 
     public void delete(int id) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_PRODUCT_QUERY)) {
-            preparedStatement.setInt(1, id);
-            preparedStatement.executeUpdate();
-            log.info("Executed: {}", preparedStatement);
-        } catch (Exception e) {
-            throw new RuntimeException("Unable to delete product from DB", e);
-        }
+        jdbcTemplate.update(DELETE_PRODUCT_QUERY, id);
     }
 
     public Optional<Product> getById(int id) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(GET_PRODUCT_BY_ID_QUERY)) {
-            preparedStatement.setInt(1, id);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                log.info("Executed: {}", preparedStatement);
-                if (resultSet.next()) {
-                    return Optional.of(PRODUCT_ROW_MAPPER.mapRow(resultSet));
-                }
-                return Optional.empty();
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("Unable to findByLogin product from DB", e);
-        }
+        return Optional.ofNullable(jdbcTemplate.queryForObject(GET_PRODUCT_BY_ID_QUERY,
+                new BeanPropertyRowMapper<>(Product.class), id));
     }
 
     public void update(Product product) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_PRODUCT_QUERY)) {
-            preparedStatement.setString(1, product.getName());
-            preparedStatement.setDouble(2, product.getPrice());
-            preparedStatement.setString(3, product.getDescription());
-            preparedStatement.setInt(4, product.getId());
-            preparedStatement.executeUpdate();
-            log.info("Executed: {}", preparedStatement);
-        } catch (Exception e) {
-            throw new RuntimeException("Unable to update product in DB", e);
-        }
+        jdbcTemplate.update(UPDATE_PRODUCT_QUERY, product.getName(), product.getPrice(),
+                product.getDescription(), product.getId());
     }
 
     public List<Product> search(String searchText) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(SEARCH_PRODUCTS_QUERY)) {
-            preparedStatement.setString(1, searchText);
-            preparedStatement.setString(2, searchText);
-            List<Product> productsSearchList = new ArrayList<>();
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                while (resultSet.next()) {
-                    Product product = PRODUCT_ROW_MAPPER.mapRow(resultSet);
-                    productsSearchList.add(product);
-                }
-            }
-            log.info("Executed: {}", preparedStatement);
-            return productsSearchList;
-        } catch (Exception e) {
-            throw new RuntimeException("Unable to search products in DB", e);
-        }
+        return jdbcTemplate.query(SEARCH_PRODUCTS_QUERY, new BeanPropertyRowMapper<>(Product.class), searchText, searchText);
     }
 }
